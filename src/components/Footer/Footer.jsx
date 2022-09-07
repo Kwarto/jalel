4c37ba90bfa1b7e415a59cf16693ad6495bd808d
Kwarto <dbitsolu@gmail.com>
2022-09-07 19:35:06 +0000

    added footer component to homepage

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
-import {FooterWrapper} from './FooterElement'
+import {FooterWrapper, FooterContentContainer, FooterArticle, ArticleWrap, Ftext, Flist, FooterCopy} from './FooterElement'
+import FooterLogo from '../../images/jalel-real-estate-logoWT.png'
 function Footer() {
   return (
-    <FooterWrapper>
-
+   <>
+     <FooterWrapper>
+      <FooterContentContainer className='container col-md-3'>
+         <FooterArticle>
+          <ArticleWrap>
+            <img src={FooterLogo} alt="" />
+            <Ftext>Jalel Real Estate is a leading firm in the real estate industry in Ghana.</Ftext>
+             <Ftext><strong>Kumasi, Ghana</strong></Ftext>
+          </ArticleWrap>
+         </FooterArticle>
+         <FooterArticle>
+          <ArticleWrap>
+            <h2>Quick Links</h2>
+            <Flist>
+            <a href="/">Home</a>
+            <a href="/">Product</a>
+            <a href="/">Categories</a>
+            <a href="/">Get Account</a>
+            </Flist>
+          </ArticleWrap>
+         </FooterArticle>
+         <FooterArticle>
+          <ArticleWrap>
+            <h2>Support</h2>
+            <Flist>
+            <a href="/">FAQs</a>
+            <a href="/">NewsLetter</a>
+            <a href="/">Terms & Conditions</a>
+            <a href="/">Privacy Policy</a>
+            </Flist>
+          </ArticleWrap>
+         </FooterArticle>
+      </FooterContentContainer>
     </FooterWrapper>
+    <FooterCopy>
+        <Ftext><strong>Jalel Real Estate &copy; 2022 | All Rights Reserved </strong></Ftext>
+    </FooterCopy>
+   </>
   )
 }
 
--- src/components/Footer/FooterElement.jsx
+++ src/components/Footer/FooterElement.jsx
@@ -1,6 +1,53 @@
 import styled from "styled-components";
 
 export const FooterWrapper = styled.div`
-    padding: 100px 0;
+    padding: 50px 0;
     background: linear-gradient(to top right, var(--main-bg-f), var(--main-bg));
+`
+
+export const FooterContentContainer = styled.div`
+ background: var(--text-color-f);
+ min-height: 60vh;
+ padding: 10px;
+`
+
+export const FooterArticle = styled.div`
+ padding-top: 15px;
+ margin-left: 10px;
+`
+
+export const ArticleWrap = styled.article`
+ h2{
+    position: relative;
+    &:after{
+        content: '';
+        position: absolute;
+        bottom: -6px;
+        left: 12px;
+        width: 25%;
+        height: 3px;
+        background: var(--main-bg-f);
+    }
+ }
+`
+export const Ftext = styled.p`
+ text-align: center;
+ padding-top: 5px;
+`
+export const Flist = styled.div`
+ margin-left: 10px;
+ a{
+    display: flex;
+    flex-flow: column;
+    padding: 10px 0;
+    color: var(--main-bg);
+    font-size: 18px;
+    font-weight: 600;
+ }
+`
+
+
+export const FooterCopy = styled.div`
+ background: var(--text-color-f);
+ padding: 20px 0;
 `
